27f0be0e2fad0a05c4a8dba1fb815b3ec37857c6
bringitph <100023329+bringitph@users.noreply.github.com>
2022-06-17 18:59:24 +0100

    Make Request ID hyperlinked in Buyers individual quotes list under a request

--- wk-woocommerce-mp-rfq/templates/front/customer/class-womprfq-customer-main-quote.php
+++ wk-woocommerce-mp-rfq/templates/front/customer/class-womprfq-customer-main-quote.php
@@ -195,7 +195,9 @@ if ( ! class_exists( 'Womprfq_Customer_Main_Quote' ) ) {
 					<tr class="woocommerce-orders-table__row woocommerce-orders-table__row--status-processing order">
 						<td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-quote-id" data-title="<?php esc_attr_e( 'Quotation Id', 'wk-mp-rfq' ); ?>">
 							<span class="<?php echo esc_attr( $ac_class ); ?>"></span>
-							<?php echo esc_html( '#' . intval( $data['id'] ) ); ?>
+							<!-- Jesse edit: Make Request ID hyperlinked in Buyers individual quotes list under a request -->
+							<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) . 'seller-quote/' . intval( $data['id'] ) ); ?>">
+							<?php echo esc_html( '' . intval( $data['id'] ) ); ?></a>
 						</td>
 						<td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-quote-product" data-title="<?php esc_attr_e( 'Product Name', 'wk-mp-rfq' ); ?>">
 							<?php echo esc_html( ( $data['seller'] ) ); ?>
